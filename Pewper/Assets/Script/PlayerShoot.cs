a27a73d7b68150db27e2144e369730377bb494c0
PieceOfKeiko <dennis_soderberg@live.se>
2019-01-18 10:46:42 +0100

    Made hej.txt and made playershoot

--- Pewper/Assets/Script/PlayerShoot.cs
+++ Pewper/Assets/Script/PlayerShoot.cs
@@ -1,13 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
 
 public class PlayerShoot : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Hej Hej
     }
 
     // Update is called once per frame
