92707b93bf4d942e8082affac706146c50ee3cd8
PieceOfKeiko <dennis_soderberg@live.se>
2019-01-18 11:52:10 +0100

    Game "works"
    You can now join a game and see other players. But the movement isnt synced.(You can move but the other players cant see you move)

---
+++ Pewper/Assets/PlayerSetup.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] compToDisable;
+
+    Camera sceneCam;
+
+    void Start()
+    {
+        
+        if (!isLocalPlayer)
+        {
+            for(int i = 0; i< compToDisable.Length; i++)
+            {
+                compToDisable[i].enabled = false;
+            }
+        }
+        else
+        {
+            sceneCam = Camera.main;
+            if (sceneCam != null)
+            {
+                sceneCam.gameObject.SetActive(false);
+            }
+        }
+
+    }
+
+    void OnDisable()
+    {
+        
+        if (sceneCam != null)
+        {
+            sceneCam.gameObject.SetActive(true);
+        }
+    }
+
+}
